d8c943d4c1aa617c061d2d0aba6600f69952e06c
ShafiulTuhin <tuhin6488@gmail.com>
2026-05-13 14:53:56 +0600

    Completed delete url

--- index.js
+++ index.js
@@ -61,6 +61,15 @@ async function run() {
       );
       res.send(result);
     });
+
+    app.delete("/destination/:id", async (req, res) => {
+      const { id } = req.params;
+      const query = {
+        _id: new ObjectId(id),
+      };
+      const result = destinationCOllection.deleteOne(query);
+      res.send(result);
+    });
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!",
